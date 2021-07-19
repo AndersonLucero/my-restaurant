4967abb83d6d0b2805a34af159378535ffa5a848
AndersonLucero <michaellucero@epn.edu.ec>
2021-07-19 14:27:56 -0500

    front end listo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { OrderComponent } from './components/order/order.component';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'orders',
+    pathMatch: 'full'
+  },
+  {
+    path: 'orders',
+    component: OrderComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
